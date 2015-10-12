7b8ac8e3957f05510b1b62317173217d652deded
Bryce Kahle <bkahle@gmail.com>
2015-10-12 12:43:37 -0700

    Remove extra length from sending and parsing of Certificate message

--- dtls.c
+++ dtls.c
@@ -1842,8 +1842,8 @@ dtls_send_certificate_ecdsa(dtls_context_t *ctx, dtls_peer_t *peer,
    * Start message construction at beginning of buffer. */
   p = buf;
 
-  dtls_int_to_uint24(p, 94);	/* certificates length */
-  p += sizeof(uint24);
+  //dtls_int_to_uint24(p, 94);	/* certificates length */
+  //p += sizeof(uint24);
 
   dtls_int_to_uint24(p, 91);	/* length of this certificate */
   p += sizeof(uint24);
@@ -2582,11 +2582,11 @@ check_server_certificate(dtls_context_t *ctx,
 
   data += DTLS_HS_LENGTH;
 
-  if (dtls_uint24_to_int(data) != 94) {
-    dtls_alert("expect length of 94 bytes for server certificate message\n");
-    return dtls_alert_fatal_create(DTLS_ALERT_DECODE_ERROR);
-  }
-  data += sizeof(uint24);
+  // if (dtls_uint24_to_int(data) != 94) {
+  //   dtls_alert("expect length of 94 bytes for server certificate message, got %i\n", dtls_uint24_to_int(data));
+  //   return dtls_alert_fatal_create(DTLS_ALERT_DECODE_ERROR);
+  // }
+  // data += sizeof(uint24);
 
   if (dtls_uint24_to_int(data) != 91) {
     dtls_alert("expect length of 91 bytes for certificate\n");
